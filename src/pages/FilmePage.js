98c4f30d7aba80c502b83eb37455ae1f984d8f77
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-11-21 16:39:12 -0300

    Maior parte do site já está feita!.

--- src/pages/FilmePage.js
+++ src/pages/FilmePage.js
@@ -1,9 +1,61 @@
-import useParams from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import styles from './FilmePage.module.css';
+import styles2 from './Home.module.css';
+import { useState, useEffect } from 'react';
+
+import api from '../services/api';
+
 
 function FilmePage() {
-    return(
-        <div>
-            Uai
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "60f04d056b064f913e494202bf2286d1",
+                    language: "pt-BR",
+                }
+            }).then((response) => {
+                setFilme(response.data);
+                console.log(response);
+                console.log('Filme: ' + filme.title);
+                setLoading(false);
+            })
+                .catch(() => {
+                    console.log("FILME NÃO ENCONTRADO!");
+                    navigate("/movies-database", { replace: true });
+                    return;
+
+                })
+        }
+
+        loadFilme();
+
+        return () => {
+            console.log("COMPONENTE FOI DESMONTADO...");
+        }
+    }, [id, navigate]);
+
+    if (loading) {
+        return (
+            <div className={styles.telaLoading}>
+                <p><h1>Sua Internet é terrivelmente lenta, aguarde enquanto carrega os dados do filme...</h1></p>
+            </div>
+        )
+    }
+
+
+    return (
+        <div className={styles.containerPageFilme}>
+            <div className={styles.pageInfo}>
+                <h1>{filme.title}</h1>
+                <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path
+                    }`} alt={filme.title} />
+            </div>
         </div>
     )
 }
